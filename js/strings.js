2bc84ca2f1295c2f51458d0ab87e2537916ab97a
Vaidmine <73877238+Vaidmine@users.noreply.github.com>
2020-11-11 16:25:51 +0200

    update

---
+++ js/strings.js
@@ -0,0 +1,37 @@
+const vardas = 'Vardenis';
+const pasisveikinimas = 'Labas rytas, Lietuva';
+
+// do not -> don't
+const neiginys = "don't";
+console.log(neiginys);
+
+// Jis pasake: "Labas".
+const fraze = 'Jis pasake: "Labas".';
+console.log(fraze);
+
+// Mother said: "Don't do it!".
+const mother1 = "Mother said: \"Don't do it!\".";
+const mother2 = 'Mother said: "Don\'t do it!".';
+console.log(mother1);
+console.log(mother2);
+
+/*
+<div>
+    <p>Lorem ipsum its amet</p>
+    <a href="#">Don't click me</a>
+</div>    
+*/
+
+const html = '<div>\
+    <p>Lorem upsum</p>\
+    <a href="#">Click me</a>\
+</div>';
+console.log(html);
+
+
+const html2 = `
+<p>Lorem ipsum its amet</p>\n\ 
+<a href="#">Don't click me</a>\ 
+</div>`;
+console.log(html2)
+
