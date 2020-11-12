0ef0bde43c586b21db21952edb39bd757f9533fc
Vaidmine <73877238+Vaidmine@users.noreply.github.com>
2020-11-12 12:24:33 +0200

    if

--- js/if.js
+++ js/if.js
@@ -4,6 +4,14 @@ naudotini: >, <, >=, <=,===, !==
 nenaudotini: ==, !=
 */
 
+/* 
+Galima struktura:
+if() {}
+if() {} else {}
+if() {} else {} ....  else if {}
+
+*/
+
 const accountBalance = 200;
 const transfer = 200;
 
@@ -24,4 +32,48 @@ if(accountBalance >= transfer){
 
 } else {
     console.log('Banko zinute: saskaitoje truksta pinigu')
+}
+
+const  akys = 'blue';
+
+if (akys == 'melynos') {
+    console.log('grazus');
+} else if (akys == 'zalios') {
+    console.log('melagiai');
+} else if (akys== 'raudonos') {
+    console.log('programuotojas');
+} else {
+    console.log('ar tu zmogus?');
+}
+
+const  akys2 = 'red';
+if (akys2 == "melynos") {
+    console.log('grazus');
+} else {
+    if (akys2 =="zalios") {
+    console.log('melagiai');
+    } else {
+    console.log('ar turi akis?');
+    }
+}
+
+// 8 + '8' = 88 ;)
+
+const pirmas = 8;
+const antras = '8';
+
+if(pirmas !== antras) {
+    console.log(true);
+}   else {
+    console.log(false);
+}
+
+//ASCII table - kiekviena raide turi savo skaiciaus kombinacijos atitinkmeni ascii lentelejeif
+const zodis1 = 'labas';
+const zodis2 = 'rytas';
+
+if(zodis1 < zodis2) {
+    console.log(true);
+}   else {
+    console.log(false);
 }
