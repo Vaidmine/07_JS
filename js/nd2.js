8f5e8012c7b7bf5ecf4be5c85f8fa573acd6c8d3
Vaidmine <73877238+Vaidmine@users.noreply.github.com>
2020-11-13 19:58:11 +0200

    mygtukas

--- js/nd2.js
+++ js/nd2.js
@@ -1,16 +1,35 @@
 console.log(123456789);
 console.log('-------------------');
 
-let i = [5,3,2,6,8,7,];
+let i = [1,5,10,15,20];
 console.log(i);
 console.log('tekstas');
 
-for(e in i) {console.log(e)};
-for(e in i) {console.log(i[e])};
-for(e in i) {console.log(i[e] * 5)};
+//"function(e)" is the event handling function (on event, object is created). "e" is the object handler (object is made accessible).
 
+for(e in i) {console.log(e)};
+console.log('A*******************');
+
+for(e in i) {console.log(i[e])};
+console.log('-------------------');
+
+for(e in i) {console.log(i[e] * 5)};
+console.log('*******************');
 for(e in i) {
-    suma += i[e] * 5; 
+    const suma = i[e] * 5; 
     console.log(i[e] * 5);
     console.log(suma);
 }
+
+function myFunction() {
+
+    let input =  document.getElementById("input").value;
+    console.log(input)
+
+    let input2 =  document.getElementById("input2").value;
+    document.getElementById("langelis").innerHTML = input + ' ' + input2 ;
+    document.body.style.backgroundColor = "aqua";
+    console.log(input2)
+  }
+
+  
