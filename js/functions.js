d7eecc6219fd061e28df0241d42fe42a5066f261
Vaidmine <73877238+Vaidmine@users.noreply.github.com>
2020-11-20 14:04:14 +0200

    aaa

--- js/functions.js
+++ js/functions.js
@@ -142,10 +142,57 @@ function skaitmenuKiekisSkaiciuje (skaic) {
     
     return kiekis;
 }
-
-
+console.log("----------------------------------");
 console.log(skaitmenuKiekisSkaiciuje(5), ' -> 1');
 console.log(skaitmenuKiekisSkaiciuje(781), ' -> 3');
 console.log(skaitmenuKiekisSkaiciuje(37060123456), ' -> 11');
 console.log(skaitmenuKiekisSkaiciuje(true), );
+console.log(skaitmenuKiekisSkaiciuje("asd"), );
 console.log(skaitmenuKiekisSkaiciuje('a'), );
+console.log(skaitmenuKiekisSkaiciuje(NaN), );
+
+console.log("----------------------------------");
+// Funkcija pavadinimu “didziausiasSkaiciusSarase”:
+// priima vieną kintamąjį
+// jei perduotas kintamasis nėra sąrašo tipo, tai išveda pranešimą “Pateikta netinkamo tipo reikšmė.”
+// jei sąrašas yra tuščias, tai išveda pranešimą “Pateiktas sąrašas negali būti tuščias.”
+// priešingu atveju, funkcija tęsia darbą
+// pereina per visą pateiktą sąrašą ir į atskirą kintamąjį įsimena skaičių, kuris tuo metu yra didžiausias
+// gražina didžiausią surastą skaičių
+// TESTAI:
+// console.log( didziausiasSkaiciusSarase( [ 1 ] ) );
+// rezultatas: 1
+// console.log( didziausiasSkaiciusSarase( [ 1, 2, 3 ] ) );
+// rezultatas: 3
+// console.log( didziausiasSkaiciusSarase( [ -5, 78, 14, 0, 18 ] ) );
+// rezultatas: 78
+// console.log( didziausiasSkaiciusSarase( [ 69, 69, 69, 69, 66 ] ) );
+// rezultatas: 69
+// console.log( didziausiasSkaiciusSarase( [ -1, -2, -3, -4, -5, -6, -7, -8 ] ) );
+// rezultatas: -1
+// console.log( didziausiasSkaiciusSarase( “pomidoras” ) );
+// rezultatas: “Pateikta netinkamo tipo reikšmė.”
+// console.log( didziausiasSkaiciusSarase( [] ) );
+// rezultatas: “Pateiktas sąrašas negali būti tuščias.”
+
+function didziausiasSkaiciusSarase(sarasas){
+    if (typeof sarasas!== 'object') {
+        return "Pateikta netinkamo tipo reikšmė."
+    }
+const size = sarasas.length;
+
+
+if (size ===0) {
+    return "Pateiktas sąrašas negali būti tuščias.";
+    
+}
+let bigNum = sarasas[0];
+for (let i = 0; i < size; i++) {
+    const skaicius = sarasas[i];
+    if (type skaicius !== "number") {
+        continue;   
+    }
+    if (bigNum < sarasas[i]){
+        bigNum = sarasas[i];
+    }
+    
